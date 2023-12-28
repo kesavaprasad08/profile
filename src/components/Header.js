b4451b7754ceac828b67af789215750fd026127e
kesavaprasad08 <kesav2661998@gmail.com>
2023-12-28 21:59:16 +0530

    stying updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,7 +13,7 @@ const Header = () => {
   };
   resizeHandler();
   window.addEventListener("resize", resizeHandler);
-  let content = (
+  let desktop = (
     <div className={Classes.linkList}>
       <li className={Classes.link}>
         <a href="#top">Home</a>
@@ -32,6 +32,21 @@ const Header = () => {
       </li>
     </div>
   );
+  let mobile = (<div class={Classes.navbar}>
+
+  <div className={Classes.dropdown}>
+    <button className={Classes.dropbtn}>☰
+      <i className="fa fa-caret-down"></i>
+    </button>
+    <div class={Classes.dropdown_content}>
+      <a href="#top">Home</a>
+      <a href="#about">About</a>
+      <a href="#skills">Skills</a>
+      <a href="#portfolio">Portfolio</a>
+      <a href="#contactMe">Contact</a>
+    </div>
+  </div>
+</div>)
   return (
     <section id="Header" className={Classes.header}>
       <nav>
@@ -39,7 +54,9 @@ const Header = () => {
           <li className={Classes.name}>
             <p>KESAVA</p> <p className={Classes.second_name}>PRASAD</p>
           </li>
-          {showLinks && content}
+          {showLinks  && desktop}
+          {!showLinks  && mobile}
+
         </ul>
       </nav>
     </section>
